f313fb2971b96d697f0ab839065badb2b42d0f88
Dariusz Duleba <dariusz.duleba@nokia.com>
2019-03-02 23:43:16 +0100

    Add simple conversion from genmap to dnastat file

---
+++ mtdna/tools/genmapper_to_dnastat.py
@@ -0,0 +1,53 @@
+import csv
+import re
+from collections import OrderedDict
+
+import sys
+
+
+def convert_genmap_to_dnastat(genmap_file, dnastat_file, sample_match='tDNA\d+-\d+_(.*)_.*'):
+    lines = map(lambda line: line.decode('utf-8'), genmap_file)
+    genmap_reader = csv.DictReader(lines, dialect='excel-tab')
+    out_headers = []
+    out_rows = []
+    out_row = OrderedDict()
+    for genmap_sample in genmap_reader:
+        sample_name = genmap_sample['Sample Name']
+        print(sample_name)
+        if re.match(sample_match, sample_name):
+            if sample_name not in out_row.values():
+                if out_row:
+                    print('\t', out_row)
+                    out_row['Uwagi'] = out_row.pop('Uwagi')
+                    out_rows.append(out_row)
+                    out_headers.extend(filter(lambda a: a not in out_headers, out_row.keys()))
+                out_row = OrderedDict()
+                out_row['Numer'] = re.sub(sample_match, r'\1', sample_name)
+                out_row['Uwagi'] = sample_name
+
+            marker = genmap_sample['Marker']
+            out_row[marker] = genmap_sample['Allele 1']
+            header_name = '{}_2'.format(marker)
+            if genmap_sample['Allele 2']:
+                out_row[header_name] = genmap_sample['Allele 2']
+            else:
+                out_row[header_name] = genmap_sample['Allele 1']
+            for allel_id in range(3, 9, 1):
+                allel_name = 'Allele {}'.format(allel_id)
+                if genmap_sample[allel_name]:
+                    out_row['{}_{}'.format(marker, allel_id)] = genmap_sample[allel_name]
+    if out_row:
+        out_row['Uwagi'] = out_row.pop('Uwagi')
+        out_rows.append(out_row)
+        out_headers.extend(filter(lambda a: a not in out_headers, out_row.keys()))
+    print(out_headers)
+    print(out_rows)
+    dna_writer = csv.DictWriter(dnastat_file, out_headers)
+    dna_writer.writeheader()
+    dna_writer.writerows(out_rows)
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'rb') as genf:
+        with open('/tmp/test.out.csv', 'w') as dnaf:
+            convert_genmap_to_dnastat(genf, dnaf)
--- mtdna/www/flask_app.py
+++ mtdna/www/flask_app.py
@@ -1,7 +1,10 @@
+from io import StringIO, BytesIO
+
 from flask import Flask, request, render_template, Response, jsonify
 from werkzeug import FileWrapper
 
 from mtdna.phylotree.parse_html import get_phylo_mapping
+from mtdna.tools.genmapper_to_dnastat import convert_genmap_to_dnastat
 from mtdna.tools.ziptool import ZIPTool
 
 app = Flask(__name__)
@@ -31,7 +34,8 @@ def phylo():
 # Tools
 @app.route('/change_extension')
 def upload_file():
-    return render_template('upload.html')
+    return render_template('upload.html', action='/return_renamed', decription="""Rename file names extension to txt<br>
+      Upload zip file to get zip with renamed files""")
 
 
 @app.route("/return_renamed", methods=['POST'])
@@ -44,6 +48,32 @@ def return_renamed():
     w = FileWrapper(memory_file)
 
     res = Response(w, mimetype='application/zip', direct_passthrough=True)
+    return res
+
+
+@app.route('/convert_genmap_to_dnastat')
+def upload_convert_file():
+    return render_template('upload.html', action='/return_converted',
+                           description="""Convert genmap file to dnastat format""")
+
+
+@app.route("/return_converted", methods=['POST'])
+def return_converted():
+    f = request.files['file']
+
+    # text file required for csv
+    out_file = StringIO()
+    convert_genmap_to_dnastat(f, out_file)
+    out_file.seek(0)
+
+    # Convert output text stream to Bytes stream
+    bytes_stream = BytesIO(out_file.read().encode('utf-8'))
+
+    # FileWrapper used because of Pythonanywhere
+    # https://stackoverflow.com/questions/50087728/alternative-of-send-file-in-flask-on-pythonanywhere
+    w = FileWrapper(bytes_stream)
+
+    res = Response(w, mimetype='text/csv', direct_passthrough=True)
     return res
 
 
